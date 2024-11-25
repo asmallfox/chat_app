f3e8d99238854a7d6bac207ac5756c66e29d16e4
asmallfox <1257092503@qq.com>
2024-11-25 22:20:26 +0800

    commit

--- lib/src/pages/layout/addressBook/notice_page.dart
+++ lib/src/pages/layout/addressBook/notice_page.dart
@@ -1,5 +1,9 @@
 import 'package:chat_app/src/utils/hive_util.dart';
+import 'package:chat_app/src/widgets/avatar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:provider/provider.dart';
 
 class NoticePage extends StatefulWidget {
   const NoticePage({
@@ -11,7 +15,6 @@ class NoticePage extends StatefulWidget {
 }
 
 class _NoticePageState extends State<NoticePage> {
-
   final verifyData = UserHive.verifyData;
 
   @override
@@ -24,10 +27,40 @@ class _NoticePageState extends State<NoticePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Group Page'),
+        title: const Text('通知'),
       ),
-      body: const Center(
-        child: Text('This is the Group Page'),
+      body: ValueListenableBuilder(
+        valueListenable: UserHive.box.listenable(keys: ['verifyData']),
+        builder: (context, box, child) {
+          final verifyData = UserHive.verifyData;
+          final List verifyList = verifyData['data'];
+          print(verifyList);
+          return ListView.separated(
+            itemCount: verifyList.length,
+            separatorBuilder: (context, _) => const SizedBox(height: 20),
+            itemBuilder: (context, index) {
+              final item = verifyList[index];
+              return ListTile(
+                leading: Avatar(
+                  url: item['receiver_avatar'],
+                ),
+                title: Text(item['name'].toString()),
+                subtitle: Text(item['message'].toString()),
+                trailing: FilledButton(
+                  child: Text('同意'),
+                  style: ButtonStyle(
+                    shape: WidgetStatePropertyAll<OutlinedBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
